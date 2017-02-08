426de1794ae6b541f7dc8c13be534106b01427bc
John <jhallman5@gmail.com>
2017-02-08 11:03:37 -0800

    calculator works

--- app.js
+++ app.js
@@ -1,47 +1,71 @@
 var memory = {
-  this.initial = [],
-  this.sign;
-  this.latter = []
+  initial: [],
+  sign: [],
+  latter: [],
 }
 
-window.onload = document.querySelectorAll(".calculator-button").forEach(function(div) {
-  (div).addEventListener('click', function(){
-    (this.sign)
-    ? memory.initial.push(this.innerHTML)
-    : memory.latter.push(this.innerHTML)
-    document.querySelector('.calculator-screen').innerHTML = memory.join('')
-    console.log('memory: ' + memory.join(''))
-  })
-})
-document.querySelectorAll(".corner").forEach(function(div) {
-  (div).addEventListener('click', function(){
-    var initialNumber = document.querySelector('.calculator-screen').innerHTML
-    var secondNumber = document.querySelector('.calculator-screen').innerHTML
-    console.log("n1 " + initialNumber)
-    console.log("s2 " + memory.sign)
-    console.log("n2 " + secondNumber)
-    if (memory.sign = '+' )
-      return initialNumber + secondNumber
-    if (memory.sign = '-' )
-      return initialNumber - secondNumber
-    if (memory.sign = 'x' )
-      return initialNumber * secondNumber
-    if (memory.sign = '÷' )
-      return initialNumber / secondNumber
-  })
-})
-//
-document.querySelectorAll(".orange").forEach(function(sign) {
+document.querySelectorAll(".operator").forEach(function(sign) {
   (sign).addEventListener('click', function(){
-    var initialNumber = document.querySelector('.calculator-screen').innerHTML
-    console.log("initialNumber: " + initialNumber)
-    document.querySelector('.calculator-screen').innerHTML = ''
-    memory = []
-    memory.sign = this.innerHTML
+    memory.sign.push(this.innerHTML)
     console.log("sign: " + memory.sign)
   })
 })
 
+document.querySelectorAll(".calculator-button").forEach(function(div) {
+  (div).addEventListener('click', function(){
+    if(div.innerHTML === "AC") {
+      memory = {
+        initial: [],
+        sign: [],
+        latter: [],
+      }
+      document.querySelector('.calculator-screen').innerHTML = 0
+    }else if( memory.sign.length === 0 ){
+      memory.initial.push(this.innerHTML)
+      document.querySelector('.calculator-screen').innerHTML = memory.initial.join('')
+    }else  {
+      memory.latter.push(this.innerHTML)
+      if( memory.latter[ memory.latter.length - 1 ] === memory.sign[ memory.sign.length - 1 ])
+        memory.latter.splice(memory.latter.length - 1, 1)
+      document.querySelector('.calculator-screen').innerHTML = memory.latter.join('')
+    }
+    console.log(memory)
+    console.log(typeof div.innerHTML)
+  })
+})
+document.querySelectorAll(".corner").forEach(function(div) {
+  (div).addEventListener('click', function(){
+    var value1 = parseFloat(memory.initial.join(''))
+    var value2 = parseFloat(memory.latter.join(''))
+    switch (memory.sign[0]) {
+      case '+':
+        document.querySelector('.calculator-screen').innerHTML = add(value1, value2);
+        break;
+      case '–':
+        document.querySelector('.calculator-screen').innerHTML = subtract(value1, value2);
+        break;
+      case 'x':
+        document.querySelector('.calculator-screen').innerHTML = multiply(value1, value2);
+        break;
+      case '÷':
+        document.querySelector('.calculator-screen').innerHTML = divide(value1, value2);
+        break;
+      }
+  })
+})
+
+function add(a, b) {
+  return a + b
+}
+function subtract(a, b) {
+  return a - b
+}
+function multiply(a, b) {
+  return a * b
+}
+function divide(a, b){
+  return a / b
+}
 
 // function addtion(currentNumber, nextNumber){
 //   var currentNumber = document.querySelector('.calculator-screen').innerHTML
